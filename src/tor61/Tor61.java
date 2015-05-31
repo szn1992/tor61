dd6bd500d208386c84ad5ade72724d30d700be56
Qingyi Fan <fqyanna@gmail.com>
2015-05-30 23:44:59 -0700

    update

--- src/tor61/Tor61.java
+++ src/tor61/Tor61.java
@@ -1,5 +1,7 @@
 package tor61;
 
+import java.util.ArrayList;
+
 public class Tor61 {
 
 	static int PORT;
@@ -11,8 +13,9 @@ public class Tor61 {
 		if (args.length != 3) {
 	    	System.err.println("Usage: java Tor61 <group number> <instance number> <HTTP Proxy port>");
 	        System.exit(1);
-	    }
+	   }
 		
+		// get info from arguments
 		GROUP = args[0];
 		INSTANCE_NUM  = Integer.valueOf(args[1]);
 		PORT = Integer.valueOf(args[2]);
@@ -21,9 +24,10 @@ public class Tor61 {
 		
 		String instanceNum_s = Util.padding(INSTANCE_NUM, 4);
 		
+		// register and fetch
 		int data = (Integer.valueOf(GROUP) << 16) | INSTANCE_NUM;
 	   Util.register("" + PORT, NAME + instanceNum_s, "" + data);
-	
+	   ArrayList<String> availableRouter = Util.fetch(NAME + instanceNum_s);
     }
 
 }
